7bbc687a5d3745f899076bc06aac31085c0f3479
Eroica-cpp <eroicacmcs@gmail.com>
2014-06-08 00:00:40 +0800

    今天工作告一段落
    能够解析html、分词，明天应该可以完成提取文本特征的工作。

--- text-classifier/extract_features.py
+++ text-classifier/extract_features.py
@@ -1,3 +1,4 @@
+ # -*- coding: utf-8 -*
 """
 This script extracts feature from raw html files
 Step 1: parse raw html files by to structured ones
@@ -6,6 +7,9 @@ Step 3: extract features
 """
 from stripHTMLTags import stripHTMLTags
 import re
+import jieba
+import nltk
+import string
 
 def load(path):
 	f = open(path)
@@ -15,14 +19,27 @@ def load(path):
 
 def strip_tags(raw_html):
 	## strip html tags
-	not_html = stripHTMLTags(raw_html.decode("gbk"))
+	no_html = stripHTMLTags(raw_html.decode("gbk"))
 	## strip javascript tags
-	not_js = re.sub(r"[A-Za-z\(\)_\s;]*{.*}[A-Za-z\(\)_\s;]*", " ", not_html)
-	
-	plain_text = not_js
+	no_js = re.sub(r"[A-Za-z\(\)_\s;]*{.*}[A-Za-z\(\)_\s;]*", " ", no_html)
+	## strip multiple spaces, 
+	plain_text = re.sub(r"\s+", " ", no_js)
+	## strip punctuation
+	delEStr = list(string.punctuation + string.digits)
+	delCStr = list("《》——（）&%￥#@！【】。，；“”：".decode("utf-8"))
+	del_list = delEStr + delCStr
+
+	plain_text = "".join([i for i in plain_text if i not in del_list])
 	return plain_text
 
 def extract_features(plain_text):
+	words = list(jieba.cut(plain_text))
+	freqdist = nltk.FreqDist(words)
+	
+	for (word, freq) in freqdist.items():
+		if word != " ":
+			print word, freq
+	#print " ".join(jieba.cut(plain_text))
 	return ""
 
 def main():
@@ -31,9 +48,7 @@ def main():
 
 	raw_html = load(path)
 	plain_text = strip_tags(raw_html)
-	print plain_text
 	features = extract_features(plain_text)
-
 
 if __name__ == "__main__":
 	main()
