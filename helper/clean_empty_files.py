20d0054a430463c82c5ae0e1d01434519dea9971
Eroica-cpp <eroicacmcs@gmail.com>
2014-06-07 20:25:36 +0800

    update

--- helper/clean_empty_files.py
+++ helper/clean_empty_files.py
@@ -7,11 +7,14 @@ def main():
 	path = "../dedicated-crawlers/raw_html/"
 	files = os.listdir(path)
 	
+	counter = 0
 	for i in files:
 		## check if a file is empty
 		## if so, delete the file
 		if os.stat(path + i)[6] == 0:
 			os.system("rm %s" % (path + i) )
+			counter += 1
+			print "clean empty file:", i, "counter =", counter
 
 if __name__ == "__main__":
 	main()
