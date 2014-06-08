a69d1442f21713987003d6996b9dc85289637f82
Eroica-cpp <eroicacmcs@gmail.com>
2014-06-08 12:29:20 +0800

    extract frequent list

--- text-classifier/extract_features.py
+++ text-classifier/extract_features.py
@@ -10,6 +10,9 @@ import re
 import jieba
 import nltk
 import string
+import os
+import sys
+import cPickle
 
 def load(path):
 	f = open(path)
@@ -17,39 +20,67 @@ def load(path):
 	f.close()
 	return doc
 
-def strip_tags(raw_html):
-	## strip html tags
-	no_html = stripHTMLTags(raw_html.decode("gbk"))
-	## strip javascript tags
-	no_js = re.sub(r"[A-Za-z\(\)_\s;]*{.*}[A-Za-z\(\)_\s;]*", " ", no_html)
-	## strip multiple spaces, 
-	plain_text = re.sub(r"\s+", " ", no_js)
-	## strip punctuation
-	delEStr = list(string.punctuation + string.digits)
-	delCStr = list("《》——（）&%￥#@！【】。，；“”：".decode("utf-8"))
-	del_list = delEStr + delCStr
+def save(dict, directory):
+	f = open(directory + "data.pickle", "w")
+	cPickle.dump(dict, f)
+	f.close()
+	return
 
-	plain_text = "".join([i for i in plain_text if i not in del_list])
+def strip_tags(raw_html):
+	plain_text = ""
+	try:
+		## strip html tags
+		no_html = stripHTMLTags(raw_html.decode("gbk"))
+		## strip javascript tags
+		no_js = re.sub(r"[A-Za-z\(\)_\s;]*{.*}[A-Za-z\(\)_\s;]*", " ", no_html)
+		## strip multiple spaces, 
+		plain_text = re.sub(r"\s+", " ", no_js)
+		## strip punctuation
+		delEStr = list(string.punctuation + string.digits)
+		delCStr = list("《》——（）&%￥#@！【】。，；“”：".decode("utf-8"))
+		del_list = delEStr + delCStr
+	except UnicodeDecodeError:
+		print "'gbk' codec can't decode bytes in position 6746-6747: illegal multibyte sequence"
+
+	tmp_text = ""
+	for i in plain_text:
+		if i not in del_list:
+			tmp_text += i
+		else:
+			tmp_text += " "
+	plain_text = tmp_text
 	return plain_text
 
-def extract_features(plain_text):
+def get_freqdist(plain_text):
 	words = list(jieba.cut(plain_text))
 	freqdist = nltk.FreqDist(words)
+	return freqdist
+
+def get_dict_pool(directory = "../data/board/"):
+	files = sorted(os.listdir(directory))
+	dict_pool = {}
+	counter = 0
+	for f in files:
+		iden = int(f.split(".")[0])
+		raw_html = load(directory + f)
+		plain_text = strip_tags(raw_html)
+		freqdist = get_freqdist(plain_text)
+		dict_pool[iden] = freqdist
+		counter += 1
+		print f, "counter =", counter
+
+	print "saving dict_pool..."
+	save(dict_pool, directory)
+	print "saved!"
 	
-	for (word, freq) in freqdist.items():
-		if word != " ":
-			print word, freq
-	#print " ".join(jieba.cut(plain_text))
-	return ""
-
 def main():
-	iden = 276109
-	path = "../data/board/%d.html" % iden
+	# iden = 276109
+	# path = "../data/board/%d.html" % iden
+	pass
+	
 
-	raw_html = load(path)
-	plain_text = strip_tags(raw_html)
-	features = extract_features(plain_text)
+
 
 if __name__ == "__main__":
-	main()
+	get_dict_pool()
 
